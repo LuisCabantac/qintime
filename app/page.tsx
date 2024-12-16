fa9b2a36ca8e4031c6f12db729df9531431a6aec
LuisCabantac <151472373+LuisCabantac@users.noreply.github.com>
2024-12-16 14:23:16 +0800

    Style the home navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
-import { auth } from "@/lib/auth";
-import { signOutAction } from "@/lib/auth-actions";
 import { Metadata } from "next";
 import { redirect } from "next/navigation";
+
+import { auth } from "@/lib/auth";
+import { signOutAction } from "@/lib/auth-actions";
+
+import Logo from "@/components/Logo";
 
 export const metadata: Metadata = {
   title: "Home | QInTime",
@@ -12,11 +15,18 @@ export default async function Home() {
   if (!session) redirect("/signin");
 
   return (
-    <div>
-      <p>HOME</p>
-      <form action={signOutAction}>
-        <button type="submit">Sign out</button>
-      </form>
-    </div>
+    <section>
+      <nav className="flex items-center justify-between border-b border-[#e9ecef] px-4 py-2">
+        <Logo size="w-8" />
+        <form action={signOutAction}>
+          <button
+            type="submit"
+            className="rounded-md bg-[#dee2e6] px-4 py-2 font-medium hover:bg-[#ced4da]"
+          >
+            Sign out
+          </button>
+        </form>
+      </nav>
+    </section>
   );
 }
