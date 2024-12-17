32fa1484666799ba7cf5ef7233bb27d468a25a8f
LuisCabantac <151472373+LuisCabantac@users.noreply.github.com>
2024-12-17 16:05:14 +0800

    Add filtering of sections

--- components/AttendeesSection.tsx
+++ components/AttendeesSection.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Session } from "next-auth";
 import { deleteAttendee } from "@/lib/user-actions";
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
@@ -9,6 +9,7 @@ import { IAttendee } from "@/lib/data-service";
 
 import UserCard from "@/components/UserCard";
 import UserForm from "@/components/UserForm";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
 
 export default function AttendeesSection({
   session,
@@ -20,14 +21,24 @@ export default function AttendeesSection({
     query: string,
   ) => Promise<IAttendee[] | null>;
 }) {
+  const router = useRouter();
+  const pathname = usePathname();
   const queryClient = useQueryClient();
+  const searchParams = useSearchParams();
   const [search, setSearch] = useState("");
   const [showUserForm, setShowUserForm] = useState(false);
+  const [filter, setFilter] = useState("all");
 
   const { data: attendees, isPending: attendeesIsPending } = useQuery({
     queryKey: ["attendees", search],
     queryFn: () => onGetAllAttendees(session?.user?.id ?? "", search),
   });
+
+  useEffect(() => {
+    const params = new URLSearchParams(searchParams);
+    params.set("filter", filter);
+    router.push(`${pathname}?${params.toString()}`);
+  }, [filter, pathname, router, searchParams]);
 
   const { mutate: handleDeleteAttendee, isPending: deleteAttendeeIsPending } =
     useMutation({
@@ -54,18 +65,38 @@ export default function AttendeesSection({
       ) : (
         <>
           <div className="mb-2 flex items-center justify-between gap-4 px-4 md:px-2">
-            <input
-              type="search"
-              placeholder="Search..."
-              className="rounded-lg border border-[#868e96] bg-transparent px-4 py-2 disabled:cursor-not-allowed"
-              onChange={(event) => setSearch(event.target.value)}
-            />
+            <select
+              className="cursor-pointer bg-transparent px-0"
+              value={filter}
+              onChange={(event) => setFilter(event.target.value)}
+            >
+              <option value="all">All</option>
+              {attendees &&
+                [...new Set(attendees?.map((attendee) => attendee.section))]
+                  .sort()
+                  .map((section) => <option key={section}>{section}</option>)}
+            </select>
             <button
               type="button"
               onClick={handleToggleShowUserForm}
               className="rounded-md bg-[#212529] px-4 py-2 font-medium text-[#dee2e6]"
             >
               Add
+            </button>
+          </div>
+          <div className="mb-2 flex items-center justify-between gap-4 px-4 md:px-2">
+            <input
+              type="search"
+              placeholder="Search..."
+              className="w-full rounded-lg border border-[#868e96] bg-transparent px-4 py-2 disabled:cursor-not-allowed"
+              onChange={(event) => setSearch(event.target.value)}
+            />
+            <button
+              type="button"
+              onClick={handleToggleShowUserForm}
+              className="w-[50%] rounded-md bg-[#e03131] px-4 py-2 font-medium text-[#dee2e6]"
+            >
+              Clear dates
             </button>
           </div>
           <ul>
@@ -94,14 +125,19 @@ export default function AttendeesSection({
                   </li>
                 ))}
             {attendees?.length
-              ? attendees?.map((attendee) => (
-                  <UserCard
-                    key={attendee.id}
-                    user={attendee}
-                    onDeleteUser={handleDeleteAttendee}
-                    deleteUserIsPending={deleteAttendeeIsPending}
-                  />
-                ))
+              ? attendees
+                  ?.filter(
+                    (attendee) =>
+                      attendee.section === filter || filter === "all",
+                  )
+                  .map((attendee) => (
+                    <UserCard
+                      key={attendee.id}
+                      user={attendee}
+                      onDeleteUser={handleDeleteAttendee}
+                      deleteUserIsPending={deleteAttendeeIsPending}
+                    />
+                  ))
               : null}
             {!attendeesIsPending && !attendees?.length ? (
               <li className="flex items-center justify-center py-10 font-medium">
